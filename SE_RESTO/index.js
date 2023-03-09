3bc359454d64a54c47698c9263e45ef65a1d9eb7
eak022 <eakeakkachai71180@gmail.com>
2023-03-09 10:42:04 +0700

    09

--- SE_RESTO/index.js
+++ SE_RESTO/index.js
@@ -61,3 +61,12 @@ var swiper = new Swiper(".review-slider", {
         }
     }
 });
+
+// Loader section
+function loader(){
+    document.querySelector(".loader-container").classList.add("fade-out")
+}
+function fadeOut(){
+    setInterval(loader, 3000)
+}
+window.onload = fadeOut;

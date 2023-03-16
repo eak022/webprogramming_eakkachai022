c058b2ef66e6422beb8f38ba9dbc5ccc92e32362
eak022 <eakeakkachai71180@gmail.com>
2023-03-16 16:51:37 +0700

    16

---
+++ Adex/Adex/assets/js/script.js
@@ -0,0 +1,40 @@
+const addEventOnElements = (elements,evenType,callback)=>{
+    for (let i = 0, len = elements.length; i<len; i++) {
+        elements[i].addEventListener(evenType, callback);
+    }
+}
+
+
+
+
+
+/**
+ * NavBar toggle
+ */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]")
+
+const toggleNavbar =()=>{
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+addEventOnElements(navTogglers,"click",toggleNavbar);
+
+/**
+ * Slider
+ */
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = (currentSlider) =>{
+    const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+    const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+    const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+    let currentSliderPo = 0;
+
+    const moveSliderItem =()=>{
+        slider
+    }
+}
